be1d85ab5fef77954f5e8ade5c69c300bffbefea
Aleksey <hudziamasuki@gmail.com>
2015-11-12 09:31:30 +0500

    posts list

--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,8 @@
 from django.views import generic
 from .models import Post
 
+
 class IndexView(generic.ListView):
-    template_name = 'index.html'
     paginate_by = 5
 
     def get_queryset(self):
