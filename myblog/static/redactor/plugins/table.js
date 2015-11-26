d46997cb50afe2ee8307e73b9ef10cba76e4a9ad
Aleksey <hudziamasuki@gmail.com>
2015-11-26 20:50:01 +0500

    table.js

--- myblog/static/redactor/plugins/table.js
+++ myblog/static/redactor/plugins/table.js
@@ -3,28 +3,23 @@
 	$.Redactor.prototype.table = function()
 	{
 		return {
-			langs: {
-				en: {
-					"table": "Table",
-					"insert-table": "Insert table",
-					"insert-row-above": "Insert row above",
-					"insert-row-below": "Insert row below",
-					"insert-column-left": "Insert column left",
-					"insert-column-right": "Insert column right",
-					"add-head": "Add head",
-					"delete-head": "Delete head",
-					"delete-column": "Delete column",
-					"delete-row": "Delete row",
-					"delete-table": "Delete table"
-				}
+			getTemplate: function()
+			{
+				return String()
+				+ '<section id="redactor-modal-table-insert">'
+					+ '<label>' + this.lang.get('rows') + '</label>'
+					+ '<input type="text" size="5" value="2" id="redactor-table-rows" />'
+					+ '<label>' + this.lang.get('columns') + '</label>'
+					+ '<input type="text" size="5" value="3" id="redactor-table-columns" />'
+				+ '</section>';
 			},
 			init: function()
 			{
 				var dropdown = {};
 
 				dropdown.insert_table = {
-									title: this.lang.get('insert-table'),
-									func: this.table.insert,
+									title: this.lang.get('insert_table'),
+									func: this.table.show,
 									observe: {
 										element: 'table',
 										in: {
@@ -37,7 +32,7 @@
 								};
 
 				dropdown.insert_row_above = {
-									title: this.lang.get('insert-row-above'),
+									title: this.lang.get('insert_row_above'),
 									func: this.table.addRowAbove,
 									observe: {
 										element: 'table',
@@ -51,7 +46,21 @@
 								};
 
 				dropdown.insert_row_below = {
-									title: this.lang.get('insert-row-below'),
+									title: this.lang.get('insert_row_below'),
+									func: this.table.addRowBelow,
+									observe: {
+										element: 'table',
+										out: {
+											attr: {
+												'class': 'redactor-dropdown-link-inactive',
+												'aria-disabled': true,
+											}
+										}
+									}
+								};
+
+				dropdown.insert_row_below = {
+									title: this.lang.get('insert_row_below'),
 									func: this.table.addRowBelow,
 									observe: {
 										element: 'table',
@@ -65,7 +74,7 @@
 								};
 
 				dropdown.insert_column_left = {
-									title: this.lang.get('insert-column-left'),
+									title: this.lang.get('insert_column_left'),
 									func: this.table.addColumnLeft,
 									observe: {
 										element: 'table',
@@ -79,7 +88,7 @@
 								};
 
 				dropdown.insert_column_right = {
-									title: this.lang.get('insert-column-right'),
+									title: this.lang.get('insert_column_right'),
 									func: this.table.addColumnRight,
 									observe: {
 										element: 'table',
@@ -93,7 +102,7 @@
 								};
 
 				dropdown.add_head = {
-									title: this.lang.get('add-head'),
+									title: this.lang.get('add_head'),
 									func: this.table.addHead,
 									observe: {
 										element: 'table',
@@ -107,7 +116,7 @@
 								};
 
 				dropdown.delete_head = {
-									title: this.lang.get('delete-head'),
+									title: this.lang.get('delete_head'),
 									func: this.table.deleteHead,
 									observe: {
 										element: 'table',
@@ -121,7 +130,7 @@
 								};
 
 				dropdown.delete_column = {
-									title: this.lang.get('delete-column'),
+									title: this.lang.get('delete_column'),
 									func: this.table.deleteColumn,
 									observe: {
 										element: 'table',
@@ -135,7 +144,7 @@
 								};
 
 				dropdown.delete_row = {
-									title: this.lang.get('delete-row'),
+									title: this.lang.get('delete_row'),
 									func: this.table.deleteRow,
 									observe: {
 										element: 'table',
@@ -148,8 +157,8 @@
 									}
 								};
 
-				dropdown.delete_table = {
-									title: this.lang.get('delete-table'),
+				dropdown.delete_row = {
+									title: this.lang.get('delete_table'),
 									func: this.table.deleteTable,
 									observe: {
 										element: 'table',
@@ -162,37 +171,51 @@
 									}
 								};
 
+				this.observe.addButton('td', 'table');
+				this.observe.addButton('th', 'table');
 
 				var button = this.button.addBefore('link', 'table', this.lang.get('table'));
 				this.button.addDropdown(button, dropdown);
 			},
+			show: function()
+			{
+				this.modal.addTemplate('table', this.table.getTemplate());
+
+				this.modal.load('table', this.lang.get('insert_table'), 300);
+				this.modal.createCancelButton();
+
+				var button = this.modal.createActionButton(this.lang.get('insert'));
+				button.on('click', this.table.insert);
+
+				this.selection.save();
+				this.modal.show();
+
+				$('#redactor-table-rows').focus();
+
+			},
 			insert: function()
 			{
-				if (this.table.getTable())
-				{
-					return;
-				}
-
 				this.placeholder.remove();
 
-				var rows = 2;
-				var columns = 3;
-				var $tableBox = $('<div>');
-				var $table = $('<table />');
+				var rows = $('#redactor-table-rows').val(),
+					columns = $('#redactor-table-columns').val(),
+					$tableBox = $('<div>'),
+					tableId = Math.floor(Math.random() * 99999),
+					$table = $('<table id="table' + tableId + '"><tbody></tbody></table>'),
+					i, $row, z, $column;
 
-
-				for (var i = 0; i < rows; i++)
+				for (i = 0; i < rows; i++)
 				{
-					var $row = $('<tr>');
+					$row = $('<tr>');
 
-					for (var z = 0; z < columns; z++)
+					for (z = 0; z < columns; z++)
 					{
-						var $column = $('<td>' + this.opts.invisibleSpace + '</td>');
+						$column = $('<td>' + this.opts.invisibleSpace + '</td>');
 
 						// set the focus to the first td
 						if (i === 0 && z === 0)
 						{
-							$column.append(this.selection.marker());
+							$column.append(this.selection.getMarker());
 						}
 
 						$($row).append($column);
@@ -204,35 +227,58 @@
 				$tableBox.append($table);
 				var html = $tableBox.html();
 
+				this.modal.close();
+				this.selection.restore();
+
+				if (this.table.getTable()) return;
+
 				this.buffer.set();
 
-				var current = this.selection.current();
-				if ($(current).closest('li').length !== 0)
+				var current = this.selection.getBlock() || this.selection.getCurrent();
+				if (current && current.tagName != 'BODY')
 				{
-					$(current).closest('ul, ol').first().after(html);
+					if (current.tagName == 'LI') current = $(current).closest('ul, ol');
+					$(current).after(html);
 				}
 				else
 				{
-					this.air.collapsed();
-					this.insert.html(html);
+					this.insert.html(html, false);
 				}
 
 				this.selection.restore();
-				this.core.callback('insertedTable', $table);
+
+				var table = this.$editor.find('#table' + tableId);
+
+				var p = table.prev("p");
+
+				if (p.length > 0 && this.utils.isEmpty(p.html()))
+				{
+					p.remove();
+				}
+
+				if (!this.opts.linebreaks && (this.utils.browser('mozilla') || this.utils.browser('msie')))
+				{
+					var $next = table.next();
+					if ($next.length === 0)
+					{
+						 table.after(this.opts.emptyHtml);
+					}
+				}
+
+				this.observe.buttons();
+
+				table.find('span.redactor-selection-marker').remove();
+				table.removeAttr('id');
+
+				this.code.sync();
+				this.core.setCallback('insertedTable', table);
 			},
 			getTable: function()
 			{
-				var $table = $(this.selection.current()).closest('table');
+				var $table = $(this.selection.getParent()).closest('table');
 
-				if (!this.utils.isRedactorParent($table))
-				{
-					return false;
-				}
-
-				if ($table.size() === 0)
-				{
-					return false;
-				}
+				if (!this.utils.isRedactorParent($table)) return false;
+				if ($table.size() === 0) return false;
 
 				return $table;
 			},
@@ -240,15 +286,12 @@
 			{
 				this.selection.restore();
 				$table.find('span.redactor-selection-marker').remove();
-
+				this.code.sync();
 			},
 			deleteTable: function()
 			{
 				var $table = this.table.getTable();
-				if (!$table)
-				{
-					return;
-				}
+				if (!$table) return;
 
 				this.buffer.set();
 
@@ -266,70 +309,55 @@
 
 				$table.remove();
 
-
+				this.code.sync();
 			},
 			deleteRow: function()
 			{
-				var $table = this.table.getTable();
-				if (!$table)
-				{
-					return;
-				}
+			var $table = this.table.getTable();
+			if (!$table) return;
 
-				var $current = $(this.selection.current());
+			var $current = $(this.selection.getCurrent());
 
-				this.buffer.set();
+			this.buffer.set();
 
-				var $current_tr = $current.closest('tr');
-				var $focus_tr = $current_tr.prev().length ? $current_tr.prev() : $current_tr.next();
-				if ($focus_tr.length)
-				{
-					var $focus_td = $focus_tr.children('td, th').first();
-					if ($focus_td.length)
-					{
-						$focus_td.prepend(this.selection.marker());
-					}
-				}
+			var $current_tr = $current.closest('tr');
+			var $focus_tr = $current_tr.prev().length ? $current_tr.prev() : $current_tr.next();
+			if ($focus_tr.length)
+			{
+				var $focus_td = $focus_tr.children('td, th').first();
+				if ($focus_td.length) $focus_td.prepend(this.selection.getMarker());
+			}
 
-				$current_tr.remove();
-				this.table.restoreAfterDelete($table);
-			},
+			$current_tr.remove();
+			this.table.restoreAfterDelete($table);
+		},
 			deleteColumn: function()
 			{
-				var $table = this.table.getTable();
-				if (!$table)
-				{
-					return;
-				}
+			var $table = this.table.getTable();
+			if (!$table) return;
 
-				this.buffer.set();
+			this.buffer.set();
 
-				var $current = $(this.selection.current());
-				var $current_td = $current.closest('td, th');
-				var index = $current_td[0].cellIndex;
+			var $current = $(this.selection.getCurrent());
+			var $current_td = $current.closest('td, th');
+			var index = $current_td[0].cellIndex;
 
-				$table.find('tr').each($.proxy(function(i, elem)
-				{
-					var $elem = $(elem);
-					var focusIndex = index - 1 < 0 ? index + 1 : index - 1;
-					if (i === 0)
-					{
-						$elem.find('td, th').eq(focusIndex).prepend(this.selection.marker());
-					}
+			$table.find('tr').each($.proxy(function(i, elem)
+			{
+				var $elem = $(elem);
+				var focusIndex = index - 1 < 0 ? index + 1 : index - 1;
+				if (i === 0) $elem.find('td, th').eq(focusIndex).prepend(this.selection.getMarker());
 
-					$elem.find('td, th').eq(index).remove();
+				$elem.find('td, th').eq(index).remove();
 
-				}, this));
+			}, this));
 
-				this.table.restoreAfterDelete($table);
-			},
+			this.table.restoreAfterDelete($table);
+		},
 			addHead: function()
 			{
 				var $table = this.table.getTable();
-				if (!$table)
-				{
-					return;
-				}
+				if (!$table) return;
 
 				this.buffer.set();
 
@@ -348,27 +376,21 @@
 				$thead = $('<thead></thead>').append(tr);
 				$table.prepend($thead);
 
-
+				this.code.sync();
 
 			},
 			deleteHead: function()
 			{
 				var $table = this.table.getTable();
-				if (!$table)
-				{
-					return;
-				}
+				if (!$table) return;
 
 				var $thead = $table.find('thead');
-				if ($thead.size() === 0)
-				{
-					return;
-				}
+				if ($thead.size() === 0) return;
 
 				this.buffer.set();
 
 				$thead.remove();
-
+				this.code.sync();
 			},
 			addRowAbove: function()
 			{
@@ -389,14 +411,11 @@
 			addRow: function(type)
 			{
 				var $table = this.table.getTable();
-				if (!$table)
-				{
-					return;
-				}
+				if (!$table) return;
 
 				this.buffer.set();
 
-				var $current = $(this.selection.current());
+				var $current = $(this.selection.getCurrent());
 				var $current_tr = $current.closest('tr');
 				var new_tr = $current_tr.clone();
 
@@ -410,7 +429,7 @@
 
 				new_tr.find('td').html(this.opts.invisibleSpace);
 
-				if (type === 'after')
+				if (type == 'after')
 				{
 					$current_tr.after(new_tr);
 				}
@@ -419,18 +438,15 @@
 					$current_tr.before(new_tr);
 				}
 
-
+				this.code.sync();
 			},
 			addColumn: function (type)
 			{
 				var $table = this.table.getTable();
-				if (!$table)
-				{
-					return;
-				}
+				if (!$table) return;
 
 				var index = 0;
-				var current = $(this.selection.current());
+				var current = $(this.selection.getCurrent());
 
 				this.buffer.set();
 
@@ -439,10 +455,7 @@
 
 				$current_tr.find('td, th').each($.proxy(function(i, elem)
 				{
-					if ($(elem)[0] === $current_td[0])
-					{
-						index = i;
-					}
+					if ($(elem)[0] === $current_td[0]) index = i;
 
 				}, this));
 
@@ -453,7 +466,7 @@
 					var td = $current.clone();
 					td.html(this.opts.invisibleSpace);
 
-					if (type === 'after')
+					if (type == 'after')
 					{
 						$current.after(td);
 					}
@@ -464,7 +477,7 @@
 
 				}, this));
 
-
+				this.code.sync();
 			}
 		};
 	};
